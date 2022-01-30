57a866326e014e2594acb9339136054f8db302bd
Werner Glinka <werner@glinka.co>
2022-01-30 11:35:45 -0600

    fixed default prism theme display

--- gulp-tasks/get-prism-styles.js
+++ gulp-tasks/get-prism-styles.js
@@ -3,9 +3,9 @@ const rename = require("gulp-rename");
 require('dotenv');
 
 module.exports = function () {
-	const theme = process.env.PRISM_THEME
+	const theme = process.env.PRISM_THEME === "default" ? "prism" : `prism-${process.env.PRISM_THEME}`
 	console.log(`Loading Prism Style ${theme} **************************`);
-  return gulp.src(`node_modules/prismjs/themes/prism-${theme}.css`)
+  return gulp.src(`node_modules/prismjs/themes/${theme}.css`)
 		.pipe(rename("prism-styles.css"))
 		.pipe(gulp.dest('src/assets/styles'));
 };
